f6ccc1852168846dd05b525869e32f0580ede0bd
aryanraj.is18@bmsce.ac.in <aryanraj.is18@bmsce.ac.in>
2022-05-04 06:31:20 +0000

    rechange

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,8 +1,8 @@
 with customers as (
-    select * from analytics.dbt_arout.stg_customers
+    select * from {{ ref('stg_customers')}}
 ),
 orders as (
-    select * from analytics.dbt_arout.fct_orders
+    select * from {{ ref('fct_orders')}}
 ),
 customer_orders as (
     select
